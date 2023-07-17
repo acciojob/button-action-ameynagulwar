02da271deb40a7f9188e4b0d83ea27a6f0948266
ameynagulwar <130839577+ameynagulwar@users.noreply.github.com>
2023-07-17 09:24:15 +0530

    submitting code-Mon Jul 17 2023 09:24:13 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,14 @@ import './../styles/App.css';
 
 const App = (props) => {
 
+  function showPara(){
+     document.getElementById('para').style.display = "block"
+  }
+
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+       <p className="hide show" id="para" style={{display: "none"}}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+       <button id="click" onClick={showPara}>Click</button>
     </div>
   );
 }
